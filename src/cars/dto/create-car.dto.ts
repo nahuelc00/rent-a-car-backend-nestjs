f6d9036fce47255d1803a635bcf66f1926df7a01
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-11 10:51:27 -0300

    Create dto's for when the car is received at the controller when saving and updating

---
+++ src/cars/dto/create-car.dto.ts
@@ -0,0 +1,10 @@
+export class CreateCarDto {
+  brand: string;
+  year: number;
+  color: string;
+  passengers: number;
+  model: string;
+  kilometers: number;
+  airConditioning: boolean;
+  transmission: string;
+}
---
+++ src/cars/dto/update-car.dto.ts
@@ -0,0 +1,14 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCarDto } from './create-car.dto';
+
+export class UpdateCarDto extends PartialType(CreateCarDto) {
+  id: string;
+  brand?: string;
+  year?: number;
+  color?: string;
+  passengers?: number;
+  model?: string;
+  kilometers?: number;
+  airConditioning?: boolean;
+  transmission?: string;
+}

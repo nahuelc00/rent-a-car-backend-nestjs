2f5c4c473d40877448b089f659ffa168c96765c4
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-11 10:48:24 -0300

    Create dto's for when the car is saved and updated in the db

---
+++ src/cars/dto/create-car-to-db.dto.ts
@@ -0,0 +1,10 @@
+export class CreateCarToDbDto {
+  brand: string;
+  year: number;
+  color: string;
+  passengers: number;
+  model: string;
+  kilometers: number;
+  air_conditioning: boolean;
+  transmission: string;
+}

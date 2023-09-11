da8e8037b0c77ce87188f634a735c8ce0b95a5fc
Nahuel <nahuel.lorenzodev@gmail.com>
2023-09-11 10:54:57 -0300

    Add configuratio in module of app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CarsModule } from './cars/cars.module';
+import { CarsController } from './cars/cars.controller';
+import { CarsService } from './cars/cars.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Car } from './cars/entities/car.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'cars.db',
+      entities: [Car],
+      synchronize: true,
+    }),
+    CarsModule,
+  ],
+  controllers: [CarsController],
+  providers: [CarsService],
 })
 export class AppModule {}

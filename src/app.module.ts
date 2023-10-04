7cc4aca9411c11be6dbee3a97f0fc9ec29bff4a6
Nahuel <nahuel.lorenzodev@gmail.com>
2023-10-04 18:31:04 -0300

    Apply new config in module of app

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,20 @@ import { CarsController } from './cars/cars.controller';
 import { CarsService } from './cars/cars.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Car } from './cars/entities/car.entity';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'cars.db',
       entities: [Car],
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: 'uploads',
     }),
     CarsModule,
   ],

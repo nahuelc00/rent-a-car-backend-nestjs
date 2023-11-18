291d56f52cd086a6f37d76ba3c327ee4d113b17b
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-18 13:22:42 -0300

    Delete propertys optionals and imageUrl property in update car dto

--- src/cars/dto/update-car.dto.ts
+++ src/cars/dto/update-car.dto.ts
@@ -3,15 +3,14 @@ import { CreateCarDto } from './create-car.dto';
 
 export class UpdateCarDto extends PartialType(CreateCarDto) {
   id: string;
-  brand?: string;
-  year?: number;
-  color?: string;
-  passengers?: number;
-  model?: string;
-  kilometers?: number;
-  airConditioning?: 'yes' | 'no';
-  imageUrl?: string;
-  transmission?: string;
+  brand: string;
+  year: number;
+  color: string;
+  passengers: number;
+  model: string;
+  kilometers: number;
+  airConditioning: 'yes' | 'no';
+  transmission: string;
   unitPrice: number;
   totalPrice: number;
 }

a48f707bd361b3f4e9712f3fa18ebecc53c82b96
Nahuel Lorenzo <nahuel.lorenzodev@gmail.com>
2023-11-18 13:23:30 -0300

    Create car-to-db interface

---
+++ src/cars/interfaces/car-to-db.interface.ts
@@ -0,0 +1,13 @@
+export interface ICarToDb {
+  brand: string;
+  year: number;
+  color: string;
+  passengers: number;
+  model: string;
+  kilometers: number;
+  airConditioning: 'yes' | 'no';
+  imageUrl: string;
+  transmission: string;
+  unitPrice: number;
+  totalPrice: number;
+}
